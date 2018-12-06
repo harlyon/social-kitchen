e3d56f212f979c9c6fad3d69c3f31d304160e640
mattrivera <rivera182matty@gmail.com>
2018-12-05 23:15:25 -0500

    firebase deploy

--- cooking-party/src/firebase.js
+++ cooking-party/src/firebase.js
@@ -1,17 +1,5 @@
 import firebase from 'firebase';
 
-// var config = {
-//   apiKey: "AIzaSyAS1abiWo5s9dkd3nYV2OFNh8m9Q4dpj2Y",
-//   authDomain: "cooking-party.firebaseapp.com",
-//   databaseURL: "https://cooking-party.firebaseio.com",
-//   projectId: "cooking-party",
-//   storageBucket: "",
-//   messagingSenderId: "409593497714"
-// };
-
-// firebase.initializeApp(config);
-
-
 var config = {
   apiKey: "AIzaSyCmITKMzPgouetH1pbECwvEC2__i1HYD14",
   authDomain: "social-kitchen-f0d8c.firebaseapp.com",
@@ -21,9 +9,5 @@ var config = {
   messagingSenderId: "519935737387"
 };
 firebase.initializeApp(config);
-
-
-
-
 
 export default firebase;
