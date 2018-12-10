254e2c4a79b6ba666df6a8264666ef332270c2b7
Teresa <teevien@hotmail.com>
2018-12-10 17:44:17 -0500

    push again

--- cooking-party/src/App.js
+++ cooking-party/src/App.js
@@ -56,7 +56,7 @@ class App extends Component {
         <div className="App">
           <header className="header">
             <div className="wrapper">
-              <NavLink to="/">
+              <NavLink to="/" className="mainTitle-link">
                 <h1 className="mainTitle">Social Kitchen</h1>
               </NavLink>
               <div>
