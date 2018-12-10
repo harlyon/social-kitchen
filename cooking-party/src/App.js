772ff266cb17381537a8522b616a53f82eb4bda4
jonathanhoy <jonathanhoy5@gmail.com>
2018-12-09 22:17:17 -0500

    styling header

--- cooking-party/src/App.js
+++ cooking-party/src/App.js
@@ -48,35 +48,98 @@ class App extends Component {
         })
       })
   }
+  // render() {
+  //   return (
+  //     <Router>
+  //       <div className="App">
+  //         {
+  //           this.state.user
+  //           ?
+  //           <button onClick={this.logOut}>Log Out</button>
+  //           :
+  //           <button onClick={this.logIn}>Log In</button>
+  //         }
+  //         {
+  //           this.state.user
+  //           ?
+  //           (
+  //             <div>
+  //               <header>
+  //                 <h1>Social Kitchen</h1>
+  //                 <NavLink to="/">Home</NavLink>
+  //                 <h2>Hello {this.state.user.displayName}!</h2>
+  //               </header>
+  //               <Route exact path="/" component={CreateEvent} />
+  //               <Route exact path={'/:party_id'} render={(props) => <EventDetails {...props} user={this.state.user} />} />
+  //               <Route exact path={'/:party_id/dishes/:dish_id'} render={(props) => <ShowDishDetailsInEvent {...props} />} />
+  //               <Route path={'/party/:party_id/:recipe_id'} render={(props) => <PrintSingleRecipe {...props} />} />
+  //             </div>
+  //             )
+  //           :
+  //           <p>You must be logged in.</p>
+  //           }
+  //       </div>
+  //     </Router>
+  //   );
+  // }
   render() {
     return (
       <Router>
         <div className="App">
-          <h1>Social Kitchen</h1>
-          {
-            this.state.user
-            ?
-            <button onClick={this.logOut}>Log Out</button>
-            :
-            <button onClick={this.logIn}>Log In</button>
-          }
-          {
-            this.state.user
-            ?
-            (
+          <header className="header">
+            <div className="wrapper">
+              <h1 className="mainTitle">Social Kitchen</h1>
+              <nav className="nav__mobile-button-container">
+                <NavLink to="/">
+                  <button className="nav__mobile-button">Home</button>
+                </NavLink>
+                <button onClick={this.logOut} className="nav__mobile-button">Log Out</button>
+              </nav>
               <div>
-                <header>
-                  <NavLink to="/">Home</NavLink>
-                  <h2>Hello {this.state.user.displayName}!</h2>
-                </header>
+                {
+                  this.state.user
+                  ?
+                  (
+                    <nav className="nav clearfix">
+                      <h2 className="nav__greeting">Hello <span>{this.state.user.displayName}</span>!</h2>
+                      <div className="nav__button-container">
+                        <NavLink to="/">
+                          <button className="nav__button">Home</button>
+                        </NavLink>
+                        <button onClick={this.logOut} className="nav__button">Log Out</button>
+                      </div>
+                      <i className="fas fa-bars nav__mobile-menu"></i>
+                      {/* <div className="nav__mobile-button-container">
+                        <NavLink to="/">
+                          <button className="nav__mobile-button">Home</button>
+                        </NavLink>
+                        <button onClick={this.logOut} className="nav__mobile-button">Log Out</button>
+                      </div> */}
+                    </nav>
+                  )
+                  :
+                  (
+                    <nav className="nav clearfix">
+                      <h2 className="nav__greeting">Please log in.</h2>
+                      <div className="nav__button-container">
+                        <button onClick={this.logIn} className="nav__button">Log In</button>
+                      </div>
+                    </nav>
+                  )
+                }
+              </div>
+            </div>
+          </header>
+          {
+            this.state.user &&
+            (
+              <main>
                 <Route exact path="/" component={CreateEvent} />
                 <Route exact path={'/:party_id'} render={(props) => <EventDetails {...props} user={this.state.user} />} />
                 <Route exact path={'/:party_id/dishes/:dish_id'} render={(props) => <ShowDishDetailsInEvent {...props} />} />
                 <Route path={'/party/:party_id/:recipe_id'} render={(props) => <PrintSingleRecipe {...props} />} />
-              </div>
-              )
-            :
-            <p>You must be logged in.</p>
+              </main>
+            )
           }
         </div>
       </Router>
