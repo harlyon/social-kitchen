55ca4139601aa274feb0b0c3584c550f169dd42c
jonathanhoy <jonathanhoy5@gmail.com>
2018-12-10 22:17:54 -0500

    finished styling dish details view

--- cooking-party/src/cookingParty/ShowDishDetailsInEvent.js
+++ cooking-party/src/cookingParty/ShowDishDetailsInEvent.js
@@ -22,18 +22,36 @@ class ShowDishDetailsInEvent extends Component {
       <div className="showDishDetailsInEvent">
         <div className="dish wrapper clearfix">
           <aside className="dish__aside">
-            <div className="dish__aside-wrapper">
-              <h2 className="dish__title">{this.state.currentDish.name}</h2>
-              <img src={this.state.currentDish.image} alt={this.state.currentDish.name} className="dish__image" />
-              <p className="dish__servings">Number of servings: <span>{this.state.currentDish.servings}</span></p>
-              <div className="dish__buttons-container">
-                <Link to={`/${this.props.match.params.party_id}`} className="dish__back-btn"><i className="fas fa-arrow-left"></i></Link>
-                <a href={this.state.currentDish.source} target="_blank" rel="noopener noreferrer" className="dish__directions-btn">Directions</a>
-              </div>
+            <h2 className="dish__title">{this.state.currentDish.name}</h2>
+            <img src={this.state.currentDish.image} alt={this.state.currentDish.name} className="dish__image" />
+            <p className="dish__servings">Number of servings: <span>{this.state.currentDish.servings}</span></p>
+            <div className="dish__buttons-container">
+              <Link to={`/${this.props.match.params.party_id}`} className="dish__back-btn"><i className="fas fa-arrow-left"></i></Link>
+              <a href={this.state.currentDish.source} target="_blank" rel="noopener noreferrer" className="dish__directions-btn">Directions</a>
             </div>
           </aside>
           <main className="dish__main">
             <h2 className="dish__ingredients-title">Ingredients:</h2>
+            <ul className="dish__ingredients-list">
+              {this.state.currentDish.ingredients &&
+                (
+                  this.state.currentDish.ingredients.map((ingredient) => {
+                    return (
+                      <li className="dish__ingredients-item">{ingredient}</li>
+                    )
+                  })
+                )}
+            </ul>
+            <ul className="dish__ingredients-list">
+              {this.state.currentDish.ingredients &&
+                (
+                  this.state.currentDish.ingredients.map((ingredient) => {
+                    return (
+                      <li className="dish__ingredients-item">{ingredient}</li>
+                    )
+                  })
+                )}
+            </ul>
             <ul className="dish__ingredients-list">
               {this.state.currentDish.ingredients &&
                 (
