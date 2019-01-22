18e0449d971b781ac2b052374944d859915cd29f
jonathanhoy <jonathanhoy5@gmail.com>
2019-01-21 22:54:42 -0500

    updated firebase

--- cooking-party/src/firebase/firebase.js
+++ cooking-party/src/firebase/firebase.js
@@ -1,13 +1,24 @@
 import firebase from 'firebase';
 
-// JONATHAN'S FIREBASE
+// old firebase
+// var config = {
+//   apiKey: "AIzaSyBgjx22ZYt2yRAYcE3IqDhu4mpU2JAxb2U",
+//   authDomain: "social-kitchen-e80b3.firebaseapp.com",
+//   databaseURL: "https://social-kitchen-e80b3.firebaseio.com",
+//   projectId: "social-kitchen-e80b3",
+//   storageBucket: "social-kitchen-e80b3.appspot.com",
+//   messagingSenderId: "344305376967"
+// };
+// firebase.initializeApp(config);
+
+// new firebase
 var config = {
-  apiKey: "AIzaSyBgjx22ZYt2yRAYcE3IqDhu4mpU2JAxb2U",
-  authDomain: "social-kitchen-e80b3.firebaseapp.com",
-  databaseURL: "https://social-kitchen-e80b3.firebaseio.com",
-  projectId: "social-kitchen-e80b3",
-  storageBucket: "social-kitchen-e80b3.appspot.com",
-  messagingSenderId: "344305376967"
+  apiKey: "AIzaSyAHjnnQ0vqJCFZK4xMYfLAX9tD1PquYG3s",
+  authDomain: "my-social-kitchen.firebaseapp.com",
+  databaseURL: "https://my-social-kitchen.firebaseio.com",
+  projectId: "my-social-kitchen",
+  storageBucket: "",
+  messagingSenderId: "48883199627"
 };
 firebase.initializeApp(config);
 
