75f0b6dea911f34f6b2b70897b1b0cc457949be5
jonathanhoy <jonathanhoy5@gmail.com>
2019-01-30 14:53:26 -0500

    updated display events styling

--- cooking-party/src/cookingParty/DisplayEvents.js
+++ cooking-party/src/cookingParty/DisplayEvents.js
@@ -10,7 +10,7 @@ class DisplayEvents extends Component {
       listOfCookingParties: {}
     })
   }
-  deleteParty = (e) => {
+  deleteEvent = (e) => {
     const firebaseKey = e.target.id;
     const partyRef = firebase.database().ref(`/${firebaseKey}`);
     const creator = e.target.getAttribute('data-creator');
@@ -22,26 +22,24 @@ class DisplayEvents extends Component {
   }
   render() {
     return (
-      <div className="displayEvents">
-        <h2>Events</h2>
-        {Object.entries(this.props.listOfCookingParties).map((party) => {          
+      <div className="events">
+        <h2 className="events__heading">Events</h2>
+        {Object.entries(this.props.listOfCookingParties).map((party) => {
           return (
-            <div key={party[1].key} className="singleEvent clearfix">
-
-              <div className="displayEvent">
-                <Link to={`/${party[1].key}`} className="displayEventName">
-                  {party[1].name}
-                </Link>
-              </div>
-              
-              <div className="eventDelete">
-                <button className="btn--delete" id={party[1].key} data-creator={party[1].creator}onClick={this.deleteParty}>
+            <div key={party[1].key} className="event__container">
+              <Link to={`/${party[1].key}`} className="event__link">
+                <div className="event__info-container">
+                  <h3 className="event__title">{party[1].name}</h3>
+                  <p className="event__creator">Created by: {party[1].creator}</p>
+                  <p className="event__date">Date: {party[1].date}</p>
+                </div>
+              </Link>
+              <div className="event__delete">
+                <button className="event__delete-btn" id={party[1].key} data-creator={party[1].creator}onClick={this.deleteEvent}>
                   <i className="fas fa-times" id={party[1].key} data-creator={party[1].creator}></i>
                 </button>
               </div>
-
             </div>
-
           );
         })}
       </div>
