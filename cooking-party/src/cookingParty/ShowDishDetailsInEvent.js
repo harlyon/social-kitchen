ef014b3218530e1357dccc64634e627515c44d4c
jonathanhoy <jonathanhoy5@gmail.com>
2019-02-04 17:26:37 -0500

    updated dish details styling

--- cooking-party/src/cookingParty/ShowDishDetailsInEvent.js
+++ cooking-party/src/cookingParty/ShowDishDetailsInEvent.js
@@ -19,32 +19,36 @@ class ShowDishDetailsInEvent extends Component {
   }
   render() {
     return (
-      <div className="showDishDetailsInEvent">
-        <div className="dish wrapper clearfix">
-          <h2 className="dish__title">{this.state.currentDish.name}</h2>
-          <aside className="dish__aside">
-            <img src={this.state.currentDish.image} alt={this.state.currentDish.name} className="dish__image" />
-            <p className="dish__servings"><span>Number of servings: </span> {this.state.currentDish.servings}</p>
-            <div className="dish__buttons-container">
-              <Link to={`/${this.props.match.params.party_id}`} className="dish__back-btn"><i className="fas fa-arrow-left"></i></Link>
-              <a href={this.state.currentDish.source} target="_blank" rel="noopener noreferrer" className="dish__directions-btn">Directions</a>
+      <React.Fragment>
+        <div className="wrapper">
+          <div className="dish">
+            <h2 className="dish__title">{this.state.currentDish.name}</h2>
+            <div className="dish__container">
+              <aside className="dish__aside">
+                <img src={this.state.currentDish.image} alt={this.state.currentDish.name} className="dish__image" />
+                <p className="dish__servings"><span>Number of servings: </span> {this.state.currentDish.servings}</p>
+                <div className="dish__buttons-container">
+                  <Link to={`/${this.props.match.params.party_id}`} className="dish__back-button"><i className="fas fa-arrow-left"></i></Link>
+                  <a href={this.state.currentDish.source} target="_blank" rel="noopener noreferrer" className="dish__directions-button">Directions</a>
+                </div>
+              </aside>
+              <section className="dish__main">
+                <h3 className="dish__ingredients-title">Ingredients</h3>
+                <ul className="dish__ingredients-list">
+                  {this.state.currentDish.ingredients &&
+                    (
+                      this.state.currentDish.ingredients.map((ingredient) => {
+                        return (
+                          <li className="dish__ingredients-item" key={ingredient}>{ingredient}</li>
+                        )
+                      })
+                    )}
+                </ul>
+              </section>
             </div>
-          </aside>
-          <section className="dish__main">
-            <h2 className="dish__ingredients-title">Ingredients:</h2>
-            <ul className="dish__ingredients-list">
-              {this.state.currentDish.ingredients &&
-                (
-                  this.state.currentDish.ingredients.map((ingredient) => {
-                    return (
-                      <li className="dish__ingredients-item" key={ingredient}>{ingredient}</li>
-                    )
-                  })
-                )}
-            </ul>
-          </section>
+          </div>
         </div>
-      </div>
+      </React.Fragment>
     )
   }
 }
