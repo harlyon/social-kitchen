53a8050fcc312d7da1b553a7477a6f63aba67cc8
jonathanhoy <jonathanhoy5@gmail.com>
2019-02-04 21:31:21 -0500

    finished responsive for create event and list events

--- cooking-party/src/cookingParty/DisplayEvents.js
+++ cooking-party/src/cookingParty/DisplayEvents.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import firebase from '../firebase/firebase';
 import { Link } from "react-router-dom";
 import swal from 'sweetalert';
+import MediaQuery from 'react-responsive';
 
 class DisplayEvents extends Component {
   constructor() {
@@ -30,15 +31,19 @@ class DisplayEvents extends Component {
               <Link to={`/${party[1].key}`} className="event__link">
                 <div className="event__info-container">
                   <h3 className="event__title">{party[1].name}</h3>
-                  <p className="event__creator">Created by: {party[1].creator}</p>
-                  <p className="event__date">Date: {party[1].date}</p>
+                  <MediaQuery query="(min-width: 451px)">
+                    <p className="event__creator">Created by: {party[1].creator}</p>
+                    <p className="event__date">Date: {party[1].date}</p>
+                  </MediaQuery>
+                  <MediaQuery query="(max-width: 450px)">
+                    <p className="event__creator">{party[1].creator}</p>
+                    <p className="event__date">{party[1].date}</p>
+                  </MediaQuery>
                 </div>
               </Link>
-              <div className="event__delete">
-                <button className="event__delete-btn" id={party[1].key} data-creator={party[1].creator}onClick={this.deleteEvent}>
-                  <i className="fas fa-times" id={party[1].key} data-creator={party[1].creator}></i>
-                </button>
-              </div>
+              <button className="event__delete-button" id={party[1].key} data-creator={party[1].creator}onClick={this.deleteEvent}>
+                <i className="fas fa-times" id={party[1].key} data-creator={party[1].creator}></i>
+              </button>
             </div>
           );
         })}
